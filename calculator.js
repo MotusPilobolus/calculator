d2c9d005c64763e220686e69d399e7b40b49f05b
Motus Pilobolus <kayla.prata@gmail.com>
2022-03-26 14:56:03 -0400

    import existing functions

---
+++ calculator.js
@@ -0,0 +1,31 @@
+const add = function(a, b) {
+	return a + b
+};
+
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(sumArray) {
+	n = sumArray.reduce(function(a, b) { return a + b; }, 0);
+  return n;
+};
+
+const multiply = function(multiplyArray) {
+  return multiplyArray.reduce( (a,b) => a * b );
+};
+
+const power = function(a,b) {
+	return Math.pow(a,b)
+};
+
+const factorial = function(num) {
+  var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
+};
