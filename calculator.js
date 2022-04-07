6f6794bd05e787afd100bdb9a267eab50c006950
Motus Pilobolus <kayla.prata@gmail.com>
2022-04-07 02:01:13 -0400

    finalize calculator scripting

--- calculator.js
+++ calculator.js
@@ -5,13 +5,17 @@ const equalsButton = document.querySelector('#equals');
 const previousNumberTextElement = document.querySelector('.previous-number');
 const operandTextElement = document.querySelector('.previous-operand');
 const currentNumberTextElement = document.querySelector('.current-number');
+var operand;
+var answer = ''
 
 //computational functions
 function add(a, b) { return a + b; }
 function subtract(a, b) { return a - b; }
-function sum(sumArray) { return sumArray.reduce(function (a, b) { return a + b; }, 0); }
-function multiply(multiplyArray) { return multiplyArray.reduce((a, b) => a * b); }
-function divide(divideArray) { return divideArray.reduce((a, b) => a / b); }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
+// function sum(sumArray) { return sumArray.reduce(function (a, b) { return a + b; }, 0); }
+// function multiplyArray(multiplyArray) { return multiplyArray.reduce((a, b) => a * b); }
+// function divideArray(divideArray) { return divideArray.reduce((a, b) => a / b); }
 
 // press functions
 function numberPress(e) {
@@ -22,9 +26,45 @@ function numberPress(e) {
 }
 
 function operandPress(e) {
-    console.log((previousNumberTextElement.textContent==''));
-    let operand = e.target.innerHTML;
+    operand = e.target.innerHTML;
+    if (previousNumberTextElement.textContent !== '' && currentNumberTextElement.textContent !== '') {
+        let a = parseInt(previousNumberTextElement.textContent);
+        let b = parseInt(currentNumberTextElement.textContent);
+        actualMath(a, b)
+        operandTextElement.textContent = operand;
+        previousNumberTextElement.textContent = answer;
+        currentNumberTextElement.textContent = '';
+    }
+    else {
     previousNumberTextElement.textContent = currentNumberTextElement.textContent;
     operandTextElement.textContent = operand;
-    currentNumberTextElement.textContent = "0"
+    currentNumberTextElement.textContent = 0; }
+}
+
+function actualMath(a, b) {
+    switch(operand) {
+        case '+':
+            answer = add(a, b);
+            break;
+         case '-':
+            answer = subtract(a, b);
+             break;
+         case 'x':
+             answer = multiply(a, b);
+             break;
+         case '/':
+             answer = divide(a, b);
+             break;
+         default:
+          console.log('error')
+    }  
+}
+
+function equalsPress(e) {
+    let a = parseInt(previousNumberTextElement.textContent);
+    let b = parseInt(currentNumberTextElement.textContent);
+  actualMath(a, b)
+  currentNumberTextElement.textContent = answer;
+  operandTextElement.textContent = ''
+  previousNumberTextElement.textContent = '';
 }
